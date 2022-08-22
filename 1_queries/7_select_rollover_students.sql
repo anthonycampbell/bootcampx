10c94ec235b9c91e75153c628d2d3ce8fee22dc4
Anthony Campbell <anthony-campbell@hotmail.com>
2022-08-22 14:39:38 -0700

    Implement join queries for summing students work time

---
+++ 1_queries/7_select_rollover_students.sql
@@ -0,0 +1,11 @@
+SELECT 
+  students.name,
+  cohorts.name,
+  cohorts.start_date AS cohort_start_date,
+  students.start_date AS student_start_date
+FROM 
+  students 
+    JOIN cohorts 
+    ON (students.cohort_id = cohorts.id)
+WHERE students.start_date != cohorts.start_date
+ORDER BY cohorts.start_date;
