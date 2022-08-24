6aea69e47915a79d1a837209988eabe1cb6c18bc
Anthony Campbell <anthony-campbell@hotmail.com>
2022-08-23 23:53:20 -0700

    Guard from sql injection

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,5 +1,9 @@
 const { Pool } = require('pg');
 const args = process.argv.slice(2);
+const values = [];
+args[0] ? values.push(`%${args[0]}%`) : values.push('FEB');
+args[1] ? values.push(Number(args[1])) : values.push(5);
+console.log(values);
 const pool = new Pool({
   user: 'vagrant',
   password: '123',
@@ -11,9 +15,9 @@ pool.query(`
 SELECT students.id, students.name AS student, cohorts.name AS cohort
 FROM students
 JOIN cohorts ON students.cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${args[0]}%'
-LIMIT ${args[1] || 5};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.student} has an id of ${user.id} and was in the ${user.cohort} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,5 +1,7 @@
 const { Pool } = require('pg');
 const args = process.argv.slice(2);
+const values = [];
+args[0] ? values.push(args[0]) : values.push('JUL02');
 const pool = new Pool({
   user: 'vagrant',
   password: '123',
@@ -13,10 +15,10 @@ FROM teachers
   JOIN assistance_requests ON teachers.id = teacher_id
   JOIN students ON students.id = student_id
   JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name = '${args[0] || 'JUL02'}'
+WHERE cohorts.name = $1
 GROUP BY teachers.id, cohorts.name
 ORDER BY teachers.name;
-`).then(res => {
+`, values).then(res => {
 
   res.rows.forEach(teacher => {
     console.log(`${teacher.cohort}: ${teacher.name}`);
